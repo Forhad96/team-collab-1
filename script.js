7e42ca104112ad0bdad8ad77e7d332d36462bf11
Shakil-Ahmmed <Shakilahmmed8882@gmail.com>
2023-07-29 15:42:47 +0600

    solved the first problem

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const harryHas = document.getElementById("harryHas").value;
-const spent = document.querySelector("#cost").value;
-const gotReturn = document.querySelector("#gotReturn").value;
+// const harryHas = document.getElementById("harryHas").value;
+// const spent = document.querySelector("#cost").value;
+// const gotReturn = document.querySelector("#gotReturn").value;
 
-console.log(spent);
+const harryHas = 5500;
+const apple = 230;
+const orange = 250;
+const toalCost = apple + orange;
+
+const sellsmanReturn = harryHas - toalCost;
+
+sellsmanReturn;
